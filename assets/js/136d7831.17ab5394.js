7292e95cfa3d3e1a2a54d48788acf43a68a23591
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-07-11 19:05:05 +0000

    Deploy website - based on 5f7fa4d1f8ce3ffca67fa3e9bee805fc4a3b95e0

---
+++ assets/js/136d7831.17ab5394.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1048],{894:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/1wairesdIndustriesWiki/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
