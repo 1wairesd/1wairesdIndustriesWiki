5440ba95d62d8a3a79ecfedb16c706fd775e96dc
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-07-11 19:52:27 +0000

    replace

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '1wairesdIndustriesWiki',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -81,7 +81,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '1wairesdIndustriesWiki',
         logo: {
           alt: 'My Site Logo',
           src: 'img/1wairesdindustries.png',
@@ -137,8 +137,12 @@ const config = {
                 to: '/blog',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: '1wairesd GitHub',
+                href: 'https://github.com/1wairesd',
+              },
+              {
+                label: '1wairesdIndustriesWiki GitHub',
+                href: 'https://github.com/1wairesd/1wairesdIndustriesWiki',
               },
             ],
           },
