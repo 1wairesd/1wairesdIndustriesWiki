e07f73a50a0c25433590b9a30f39f6fc5435bb12
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-08-03 21:32:31 +0000

    replace

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,16 +111,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                href: 'https://discord.gg/FcjketuH',
               },
             ],
           },
