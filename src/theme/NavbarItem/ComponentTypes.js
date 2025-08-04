8f02d2c287f3797f56dcf6c077ae9de448f7ac20
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-08-04 22:15:59 +0000

    Refactor Navbar component to remove custom download button and adjust styles for NavbarDownloadButton

---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import NavbarDownloadButton from '@site/src/components/NavbarDownloadButton';
+
+export default function CustomDownloadNavbarItem() {
+  return <NavbarDownloadButton />;
+} 
---
+++ src/theme/NavbarItem/index.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import CustomDownloadNavbarItem from './ComponentTypes';
+
+export default {
+  ...ComponentTypes,
+  'custom-download': CustomDownloadNavbarItem,
+}; 
